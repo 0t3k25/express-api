19d6cb99631af2773e7ec50f12dac1cea3e7836e
hureta <40241920+0t3k25@users.noreply.github.com>
2020-09-27 22:34:37 +0900

    problem

--- mywebapi/index.js
+++ mywebapi/index.js
@@ -1,24 +1,51 @@
 //expressモジュールを読み込む
 const express = require('express');
+//multerモジュールを読み込む
+const multer = require('multer');
+//uuidモジュールを読み込む
+const uuidv4 = require('uuid');
 
 //expressアプリを生成する
 const app = express();
-
+//multerでブラウザから送信されたデータを解釈する
+app.use(multer().none());
 //webフォルダの中身を公開する
 app.use(express.static('web'));
 
+const todoList = [];
+
 //http://localhost/:3000/api/v1/listにアクセスしてきたときに
 //TODOリストを返す
-app.get('/api/v1/list',(req,res) => {
-    //クライアントに送るJSONデータ
-    const todoList = [
-        {title: 'JavaScriptを勉強する',done: true},
-        {title: 'Node.jsを勉強する',done: false},
-        {title: 'Web APIを作る',done: false}
-    ];
-
+app.get('/api/v1/list',(req, res) => {
     //JSONを送信する
     res.json(todoList);
+});
+
+//http://localhost:3000/api/v1/addにデータを送信してきた時に
+//TODOリストに項目を追加する
+app.post('/api/v1/add',(req, res) => {
+    //クライアントからの送信データを取得する
+    const todoData = req.body;
+    const todoTitle = todoData.title;
+
+    //ユニークIDを生成する
+    const id = uuidv4();
+
+    //TODO項目を作る
+    const todoItem = {
+        id,
+        title: todoTitle,
+        done: false
+    };
+
+    // TODOリストに項目を追加する
+    todoList.push(todoItem);
+
+    //コンソールに出力する
+    console.log('Add: ' + JSON.stringify(todoItem));
+
+    //追加した項目をクライアントに返す
+    res.json(todoItem);
 });
 
 //ポート3000でサーバーを立てる
